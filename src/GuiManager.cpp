baae0e8cfb2f42b62d57520bdc5c22c8e7aab5cf
Jonathan FRICKERT <jonathan.frickert@epitech.eugit config --global user.email jonathan.frickert@epitech.eugit config --global user.email jonathan.frickert@epitech.eu>
2018-04-30 12:34:25 +0200

    add guiManager

---
+++ src/GuiManager.cpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_plazza
+** File description:
+** GuiManager.cpp
+*/
+
+#include "GuiManager.hpp"
+
+GuiManager::GuiManager()
+{
+
+}
+
+GuiManager::~GuiManager()
+{
+
+}
+
+void GuiManager::openRenderer()
+{
+}
+
+void GuiManager::closeRenderer()
+{
+
+}
+
+void GuiManager::refreshWindow()
+{
+
+}
