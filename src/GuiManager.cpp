bcd6371f301dd6f8ea3b8b42e1979b73d4791d38
Jonathan FRICKERT <jonathan.frickert@epitech.eugit config --global user.email jonathan.frickert@epitech.eugit config --global user.email jonathan.frickert@epitech.eu>
2018-05-07 08:21:32 +0200

    add ui

--- src/GuiManager.cpp
+++ src/GuiManager.cpp
@@ -5,6 +5,8 @@
 ** GuiManager.cpp
 */
 
+#include <unistd.h>
+#include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include "GuiManager.hpp"
@@ -39,7 +41,23 @@ void Plazza::GuiManager::setButtonWindow()
 {
 	sf::Sprite sprite(_textureImg);
 	sprite.setPosition(10, 20);
+	sprite.setScale(sf::Vector2f(0.09f, 0.09f));
+	_vector["Email Address"] = sprite.getGlobalBounds();
+	this->drawText(std::string("Email Adress"), {40, 20});
 	_window.draw(sprite);
+	sprite.setPosition(10, 50);
+	sprite.setScale(sf::Vector2f(0.09f, 0.09f));
+	_vector["Phone Number"] = sprite.getGlobalBounds();
+	this->drawText(std::string("Phone Number"), {40, 50});
+	_window.draw(sprite);
+	sprite.setPosition(10, 80);
+	sprite.setScale(sf::Vector2f(0.09f, 0.09f));
+	_vector["Ip Address"] = sprite.getGlobalBounds();
+	this->drawText(std::string("Ip Address"), {40, 80});
+	_window.draw(sprite);
+	if (!_info.empty())
+		this->drawText("Info choisi : " + _info, {10, 110});
+	this->drawText(std::string("Looking for " + _info + " on file: " + _file), {0, 200});
 }
 
 bool Plazza::GuiManager::isOpen() const
@@ -53,22 +71,110 @@ bool Plazza::GuiManager::pollEvents()
 	bool ret = false;
 
 	while (_window.pollEvent(event)) {
-		if (event.type == sf::Event::Closed) {
-			_window.close();
+		switch (event.type) {
+			case sf::Event::Closed :
+				_window.close();
+				break;
+			case sf::Event::MouseButtonPressed :
+				this->getMousePressed(sf::Vector2f(sf::Mouse::getPosition(_window)));
+				break;
+			case sf::Event::TextEntered :
+				setCmd(static_cast<char>(event.text.unicode));
+				break;
+			case sf::Event::Resized:break;
+			case sf::Event::LostFocus:break;
+			case sf::Event::GainedFocus:break;
+			case sf::Event::KeyPressed:break;
+			case sf::Event::KeyReleased:break;
+			case sf::Event::MouseWheelMoved:break;
+			case sf::Event::MouseWheelScrolled:break;
+			case sf::Event::MouseButtonReleased:break;
+			case sf::Event::MouseMoved:break;
+			case sf::Event::MouseEntered:break;
+			case sf::Event::MouseLeft:break;
+			case sf::Event::JoystickButtonPressed:break;
+			case sf::Event::JoystickButtonReleased:break;
+			case sf::Event::JoystickMoved:break;
+			case sf::Event::JoystickConnected:break;
+			case sf::Event::JoystickDisconnected:break;
+			case sf::Event::TouchBegan:break;
+			case sf::Event::TouchMoved:break;
+			case sf::Event::TouchEnded:break;
+			case sf::Event::SensorChanged:break;
+			case sf::Event::Count:break;
 		}
 	}
 	return ret;
 }
 
+void Plazza::GuiManager::drawLine()
+{
+	sf::Vertex line[] = {
+		sf::Vertex(sf::Vector2f(0, 150)),
+		sf::Vertex(sf::Vector2f(250, 150))
+	};
+	_window.draw(line, 2, sf::Lines);
+	sf::Vertex linetmp[] = {
+		sf::Vertex(sf::Vector2f(250, 0)),
+		sf::Vertex(sf::Vector2f(250, 150))
+	};
+	_window.draw(linetmp, 2, sf::Lines);
+}
+
 bool Plazza::GuiManager::guiLoop()
 {
+	this->openRenderer();
 	this->openRenderer();
 	while (this->isOpen()) {
 		_window.clear();
 		this->pollEvents();
 		this->setButtonWindow();
+		this->drawLine();
 		_window.display();
+		if (_isCmdReady) {
+			this->drawText(std::string("Find " + _info + " on file: " + _file), {200, 200});
+			_file.clear();
+			_info.clear();
+			_isCmdReady = false;
+		}
 	}
 	closeRenderer();
 	return true;
+}
+
+void Plazza::GuiManager::getMousePressed(sf::Vector2f click)
+{
+	for (auto x : _vector) {
+		if (x.second.contains(click.x, click.y))
+			_info = x.first;
+	}
+}
+
+void Plazza::GuiManager::drawText(std::basic_string<char> string, sf::Vector2f pos)
+{
+	_text.setFont(_font);
+	_text.setString(string);
+	_text.setCharacterSize(18);
+	_text.setColor(sf::Color::White);
+	_text.setPosition(pos.x, pos.y);
+	_window.draw(_text);
+}
+
+void Plazza::GuiManager::setCmd(char c)
+{
+	if (c != 0) {
+		if (c == '\r' || c == '\n')
+			_isCmdReady = true;
+		else
+			_file += c;
+	}
+}
+
+void Plazza::GuiManager::cmdReady()
+{
+	std::cout << std::string("Find " + _info + " on file: " + _file) << std::endl;
+	usleep(5000);
+	this->drawText(std::string("Find " + _info + " on file: " + _file), {60, 60});
+	_file.clear();
+	_info.clear();
 }
--- src/GuiManager.hpp
+++ src/GuiManager.hpp
@@ -8,6 +8,10 @@
 #ifndef CPP_PLAZZA_GUIMANAGER_HPP
 #define CPP_PLAZZA_GUIMANAGER_HPP
 
+#include "Keys.hpp"
+#include <iostream>
+#include <unordered_map>
+#include <map>
 #include <SFML/Graphics.hpp>
 
 namespace Plazza {
@@ -18,17 +22,65 @@ namespace Plazza {
 		void openRenderer();
 		void closeRenderer();
 		void refreshWindow();
+		void getMousePressed(sf::Vector2f click);
 		void setButtonWindow();
 		bool isOpen() const;
 		bool guiLoop();
 		bool pollEvents();
+		void drawText(std::basic_string<char> text, sf::Vector2f pos);
+		void drawLine();
 	private:
+		std::string _info = "";
+		std::map<std::string, sf::FloatRect> _vector;
 		sf::RenderWindow _window;
 		sf::Text _text;
 		sf::Font _font;
 		sf::Sprite _sprite;
 		sf::Texture _texture;
 		sf::Texture _textureImg;
+		std::string _file;
+		bool _isCmdReady = false;
+		std::unordered_map<sf::Keyboard::Key, Plazza::Keys> _keyMap = {
+			{sf::Keyboard::Key::A, Plazza::Keys::A},
+			{sf::Keyboard::Key::B, Plazza::Keys::B},
+			{sf::Keyboard::Key::C, Plazza::Keys::C},
+			{sf::Keyboard::Key::D, Plazza::Keys::D},
+			{sf::Keyboard::Key::E, Plazza::Keys::E},
+			{sf::Keyboard::Key::F, Plazza::Keys::F},
+			{sf::Keyboard::Key::G, Plazza::Keys::G},
+			{sf::Keyboard::Key::H, Plazza::Keys::H},
+			{sf::Keyboard::Key::I, Plazza::Keys::I},
+			{sf::Keyboard::Key::J, Plazza::Keys::J},
+			{sf::Keyboard::Key::K, Plazza::Keys::K},
+			{sf::Keyboard::Key::L, Plazza::Keys::L},
+			{sf::Keyboard::Key::M, Plazza::Keys::M},
+			{sf::Keyboard::Key::N, Plazza::Keys::N},
+			{sf::Keyboard::Key::O, Plazza::Keys::O},
+			{sf::Keyboard::Key::P, Plazza::Keys::P},
+			{sf::Keyboard::Key::Q, Plazza::Keys::Q},
+			{sf::Keyboard::Key::R, Plazza::Keys::R},
+			{sf::Keyboard::Key::S, Plazza::Keys::S},
+			{sf::Keyboard::Key::T, Plazza::Keys::T},
+			{sf::Keyboard::Key::U, Plazza::Keys::U},
+			{sf::Keyboard::Key::V, Plazza::Keys::V},
+			{sf::Keyboard::Key::W, Plazza::Keys::W},
+			{sf::Keyboard::Key::X, Plazza::Keys::X},
+			{sf::Keyboard::Key::Y, Plazza::Keys::Y},
+			{sf::Keyboard::Key::Z, Plazza::Keys::Z},
+			{sf::Keyboard::Key::Left, Plazza::Keys::LEFT},
+			{sf::Keyboard::Key::Right, Plazza::Keys::RIGHT},
+			{sf::Keyboard::Key::Up, Plazza::Keys::UP},
+			{sf::Keyboard::Key::Down, Plazza::Keys::DOWN},
+			{sf::Keyboard::Key::Return, Plazza::Keys::ENTER},
+			{sf::Keyboard::Key::Space, Plazza::Keys::SPACE},
+			{sf::Keyboard::Key::Delete, Plazza::Keys::DELETE},
+			{sf::Keyboard::Key::BackSpace, Plazza::Keys::BACKSPACE},
+			{sf::Keyboard::Key::Tab, Plazza::Keys::TAB},
+			{sf::Keyboard::Key::Escape, Plazza::Keys::ESC}};
+
+		void setCmd(char i);
+
+		void cmdReady();
 	};
 };
 #endif
